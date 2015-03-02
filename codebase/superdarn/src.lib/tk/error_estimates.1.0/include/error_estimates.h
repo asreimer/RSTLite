de17e3eeae6363394a566b6645e34cf55d00dac3
Ashton Reimer <ashtonsethreimer@gmail.com>
2015-03-02 02:15:55 -0600

    Removed unnecessary definition of constant PI.

--- codebase/superdarn/src.lib/tk/error_estimates.1.0/include/error_estimates.h
+++ codebase/superdarn/src.lib/tk/error_estimates.1.0/include/error_estimates.h
@@ -5,8 +5,6 @@
 #ifndef _error_estimates_H
 #define _error_estimates_H
 
-#define PI 3.14159
-
 int lag0_error(int nrang, float *pwr0, float noise_power, float K, float Kn, float *error);
 int norm_acf_error1(int nrang, float *pwr0, float noise_power, float *selfclutter, float *acf, float K, float Kn, float *error);
 int norm_acf_error2(int nrang, float *pwr0, float noise_power, float *selfclutter, float *acf, float K, float Kn, float *error);
